7c9bb2be5cb2dad5f4fd122494496a6cf5425de2
Akash A Desai <62583018+akashAD98@users.noreply.github.com>
2022-08-26 11:10:25 +0530

    Update trt.py

--- trt.py
+++ trt.py
@@ -16,7 +16,8 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("-e", "--engine", help="TRT engine Path")
     parser.add_argument("-i", "--image", help="image path")
-    parser.add_argument("-o", "--output", help="image output path")
+    parser.add_argument("-o", "--output", help="image/video output path")
+    parser.add_argument("-confi", "--conf", help="set confidence ")
     parser.add_argument("-v", "--video",  help="video path or camera index ")
     parser.add_argument("--end2end", default=False, action="store_true",
                         help="use end2end engine")
@@ -27,10 +28,12 @@ if __name__ == '__main__':
     pred = Predictor(engine_path=args.engine)
     pred.get_fps()
     img_path = args.image
+    confi=args.conf
     video = args.video
     if img_path:
-      origin_img = pred.inference(img_path, conf=0.1, end2end=args.end2end)
+      origin_img = pred.inference(img_path, confi=0.1, end2end=args.end2end)
 
       cv2.imwrite("%s" %args.output , origin_img)
     if video:
-      pred.detect_video(video, conf=0.1, end2end=args.end2end) # set 0 use a webcam
+      output_path=pred.detect_video(video, confi=0.1, end2end=args.end2end) # set 0 use a webcam
+      cv2.imwrite("%s" %args.output , output_path)
